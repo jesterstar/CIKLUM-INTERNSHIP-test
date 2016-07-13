7a5b048b03b4afc35962b6ed7644d933e0988809
Sergey <sergey.glazov@kitsoft.kiev.ua>
2016-07-13 23:28:38 +0300

    microsoft first update

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,53 @@
 /**
  * Created by Sergey on 14.06.2016.
  */
+
+// Microsoft
+
+$(document).ready(function() {
+
+    var ul       =  $('.microsoft__nav-menu ul'),
+        drop     =  $('.microsoft__dropdown-container');
+
+    $(drop).hide();
+
+    $(function() {
+
+        $(ul).on('click','li.microsoft__dropdown',function(e){
+
+            e.preventDefault();
+
+            if($(this).hasClass('active')){
+                $(this).removeClass('active').find(drop).slideUp(150);
+                return;
+            }
+
+            $(ul).find('li.active').removeClass('active').find(drop).slideUp(150);
+            $(this).addClass('active');
+            $(ul).find('li.active').find(drop).slideDown(150);
+
+        });
+    });
+
+    var subli     = $('.microsoft__dropdown-container ul li'),
+        cont      = $('.microsoft__dropdown-tab-content');
+
+    $(subli).mouseenter('li .microsoft__dropdown-tab',function(e){
+
+        e.preventDefault();
+
+        $(this).addClass('active');
+        $(subli).find(cont).css("display", "block");
+
+    });
+
+    $(subli).mouseleave('li .microsoft__dropdown-tab',function(e){
+
+        e.preventDefault();
+
+        $(this).removeClass('active');
+        $(subli).find(cont).css("display", "none");
+
+    });
+
+});
